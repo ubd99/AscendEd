d5091a2ab71588feb01ca22573f0ff9f096fa00a
ubd99 <ubaid.khan_01999@yahoo.com>
2025-05-23 16:57:19 +0500

    commit 12

--- src/Components/CourseCard.tsx
+++ src/Components/CourseCard.tsx
@@ -12,7 +12,6 @@ interface ICourseInfo{
 
 const Course_card: React.FC<ICourseInfo> = ({imgSrc, name, description, rating, className="", id=""}) =>{
     return (
-            <Link to={`/course/${id}`}>
                 <div 
                 /*style={{cursor: onClick ? 'pointer' : 'default'}}
                 role={onClick ? 'button' : undefined}
@@ -25,13 +24,13 @@ const Course_card: React.FC<ICourseInfo> = ({imgSrc, name, description, rating, 
                 }}
                 onClick={onClick}*/
                 className={`rounded-lg mx-auto md:rounded-md lg:rounded-lg xl:rounded-xl p-3 xl:p-6 w-50 md:w-70 md:h-90 lg:w-80 xl:w-90 xl:h-100 bg-purple-900 ${className}`}>
-                
+                <Link to={`/course/${id}`}>
                     <img src={imgSrc} className=" w-full h-40 sm:h-48 md:h-50 xl:h-55 rounded-xl shadow-md shadow-black"></img>
                     <p className="font-bold pt-4">{name}</p>
                     <p className="italic xl:pt-4">{description}</p>
                     <p className="xl:pt-4">{`Rating: ${rating}`}</p>        
+                </Link>
                 </div>
-            </Link>
     )
 }
 export {Course_card}
